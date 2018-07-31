f9fe468e1a7fef399b8189118ea3b603f716b39d
toxicafunk <toxicafunk@gmail.com>
2018-07-31 16:57:07 +0200

    Add syntax

--- src/main/scala/es/richweb/fs2/Intro.scala
+++ src/main/scala/es/richweb/fs2/Intro.scala
@@ -10,11 +10,27 @@ import scala.language.higherKinds
 object Intro {
   val fut: Future[Int] = Future(1)
 
-  type IO[A] = () => A
+  case class IO[A](unsafeRun: () => A) { self =>
+    def map[B](f: A => B): IO[B] = IO(() => f(self.unsafeRun()))
+    def flatMap[B](f: A => IO[B]): IO[B] = IO(() => f(self.unsafeRun()).unsafeRun())
+  }
 
   object IO {
-    def apply[A](a: => A): IO[A] =
-      () => a
+    //def apply[A](a: A): IO[A] =
+    //  new IO(() => a)
+
+    implicit val ioMonad: Monad[IO] =
+      new Monad[IO] {
+        def pure[A](a: A): IO[A] = IO(() => a)
+
+        def map[A,B](io: IO[A])(f: A => B): IO[B] =
+          io.map(f)
+
+        def flatMap[A,B](io: IO[A])(f: A => IO[B]): IO[B] =
+          io.flatMap(f)
+      }
+
+    def pure[A](a: => A): IO[A] = IO(() => a)
   }
 
   trait Functor[F[_]] {
@@ -27,33 +43,30 @@ object Intro {
   }
 
   object Monad {
-    implicit val ioMonad: Monad[IO] =
-      new Monad[IO] {
-        def pure[A](a: A): IO[A] = IO(a)
-
-        def map[A,B](io: IO[A])(f: A => B): IO[B] =
-          IO(f(io()))
-
-        def flatMap[A,B](io: IO[A])(f: A => IO[B]): IO[B] =
-          f(io())
-      }
-
-    //def map[A,B](a: A)(f: A => B)(implicit M: Monad[IO]) = M.map(IO(a))(f)
+    def apply[F[_]](implicit F: Monad[F]) = F
   }
 
+  implicit class MonadSyntax[F[_], A](fa: F[A]) {
+    def map[B](f: A => B)(implicit F: Monad[F]) = F.map(fa)(f)
+
+    def flatMap[B](f: A => F[B])(implicit F: Monad[F]) = F.flatMap(fa)(f)
+  }
+
+  def pure[F[_],A](a: => A)(implicit F: Monad[F]): F[A] = F.pure(a)
+
   def print(message: String): IO[Unit] =
-    () => println(message)
+    IO(() => println(message))
 
   def printIO(msg: String)(implicit io: Monad[IO]): IO[Unit] = 
-    io.map(IO(msg))(println)
+    IO.pure(msg).map(println)
 
   def main(args: Array[String]): Unit = {
     //fut.foreach { i => println(i + i) }
     val r = Await.result(fut, 1.second)
     def p = println(r + r)
     val io = printIO("Look ma!!!! No effect!")
-    io()
+    io.unsafeRun()
     p
-    io()
+    io.unsafeRun()
   }
 }
