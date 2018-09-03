ebe6b1f72a29450009aed0f08a2bdc2bfb979caa
Eric Rodriguez <eric.rodriguez@eniro.com>
2018-09-03 12:42:11 +0200

    Test with variance

---
+++ src/main/scala/es/richweb/fs2/Variance.scala
@@ -0,0 +1,21 @@
+package es.richweb.fs2
+
+sealed trait User { val id: Int }
+case class Member(id: Int, name: String) extends User
+case class Admin(id: Int, accss: Set[String]) extends User
+
+
+trait Consumer[-T] {
+  def consume(value: T): Unit
+}
+
+object Variance {
+  val members: List[Member] = List(Member(1, "Jan"), Member(2, "Eric"))
+
+  def consumer: Consumer[User] = (value: User) => print(s"User: ${value.name}\n")
+
+  def main(args: Array[String]): Unit = {
+    members.foreach(consumer.consume)
+  }
+
+}
